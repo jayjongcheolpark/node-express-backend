3162fe6bc29fdda2ffdb07be5b14d0044f0fcf7e
gogolearn <jay.jongcheol.park@gmail.com>
2017-09-17 20:22:02 -0400

    Finish google oauth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,18 +4,22 @@ module.exports = (app) => {
   app.get(
     '/auth/google',
     passport.authenticate('google', {
-      scope: ['profile', 'email']
+      scope: ['profile', 'email'],
+      prompt: 'select_account'
     })
   )
 
   app.get(
     '/auth/google/callback',
-    passport.authenticate('google')
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys')
+    }
   )
 
   app.get('/api/logout', (req, res) => {
     req.logout()
-    res.send(req.user)
+    res.redirect('/')
   })
 
   app.get('/api/current_user', (req, res) => {
--- services/passport.js
+++ services/passport.js
@@ -23,21 +23,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then(existingUser => {
-          if (existingUser) {
-            // we already have a record with the given profile ID
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
 
-            // 1st: error object, 2nd: user record
-            done(null, existingUser);
-          } else {
-            // we don't have a user record with this ID, make a new record
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user))
-          }
-        })
-    }
+      if (existingUser) {
+        return done(null, existingUser);
+      } 
+
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user)
+   }
   )
 )

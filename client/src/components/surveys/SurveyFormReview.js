633a9cc0689d3c7d3c9842fecc3d8d065c0c57c8
gogolearn <jay.jongcheol.park@gmail.com>
2017-09-25 21:51:03 -0400

    Finalizing review fields

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,24 @@
+import _ from 'lodash'
 import React from 'react'
+import { connect } from 'react-redux'
+import formFields from './formFields'
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ label, name }) => {
+    return (
+      <div>
+        <label>{label}</label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    )
+  }) 
+
   return (
     <div>
-      <h5>Please confirm your entries</h5> 
+      <h5>Please confirm your entries</h5>
+      {reviewFields}
       <buttom
         className="yellow darken-3 btn-flat" 
         onClick={onCancel}
@@ -14,4 +29,10 @@ const SurveyFormReview = ({ onCancel }) => {
   )
 }
 
-export default SurveyFormReview
+function mapStateToProps({ form }) {
+  return {
+    formValues: form.surveyForm.values 
+  } 
+}
+
+export default connect(mapStateToProps)(SurveyFormReview)

6bc6da30cf17e662e576cac42b2a893fd3dd43e7
gogolearn <jay.jongcheol.park@gmail.com>
2017-09-29 22:04:15 -0400

    Fix typo

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -34,7 +34,7 @@ module.exports = app => {
           }
         }, {
           $inc: { [choice]: 1 },
-          $set: { 'recipients.$.redponded': true }
+          $set: { 'recipients.$.responded': true }
         }).exec()
       })
       .value()
